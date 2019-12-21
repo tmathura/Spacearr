bfcce66bc79b7fa3a86b72492661aba307fc1889
Teshvier Mathura <teshvierm@saratoga.co.za>
2019-12-21 11:12:19 +0200

    remove add item functionality

--- Multilarr/Multilarr/Views/ItemsPage.xaml.cs
+++ Multilarr/Multilarr/Views/ItemsPage.xaml.cs
@@ -39,11 +39,6 @@ namespace Multilarr.Views
             ItemsListView.SelectedItem = null;
         }
 
-        async void AddItem_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
-        }
-
         protected override void OnAppearing()
         {
             base.OnAppearing();

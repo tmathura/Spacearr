6a6470a40378ae782d8d4831959fd2f1ce12de68
Teshvier Mathura <teshvierm@saratoga.co.za>
2020-03-06 17:02:47 +0200

    add ComputersDrivesTests

---
+++ Multilarr.Common.Tests/WindowsDrivesTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Multilarr.Common.Interfaces;
+using Multilarr.Common.Tests.Factories;
+
+namespace Multilarr.Common.Tests
+{
+    [TestClass]
+    public class ComputersDrivesTests
+    {
+        private const int NoOfDriveInfos = 3;
+        private Mock<IComputerDriveInfo> _mockIComputersDriveInfo;
+        private ComputerDrives _computersDrives;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _mockIComputersDriveInfo = new Mock<IComputerDriveInfo>();
+            _mockIComputersDriveInfo.Setup(x => x.GetComputerDrives()).Returns(ComputerDriveInfoFactory.CreateComputerDriveInfos(NoOfDriveInfos));
+            _computersDrives = new ComputerDrives(_mockIComputersDriveInfo.Object);
+        }
+
+        [TestMethod]
+        public void GetDrives()
+        {
+            // Act
+            var drives = _computersDrives.GetComputerDrives();
+
+            // Assert
+            Assert.AreEqual(NoOfDriveInfos, drives.Count);
+        }
+    }
+}

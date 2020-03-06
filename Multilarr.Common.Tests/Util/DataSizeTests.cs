8c8b61f7ee00f72f5394a7ef3b1df889ead18983
Teshvier Mathura <teshvierm@saratoga.co.za>
2020-03-06 22:04:44 +0200

    refactor DataSizeTests

--- Multilarr.Common.Tests/Util/DataSizeTests.cs
+++ Multilarr.Common.Tests/Util/DataSizeTests.cs
@@ -7,69 +7,20 @@ namespace Multilarr.Common.Tests.Util
     public class DataSizeTests
     {
 
-        [TestMethod]
-        public void SizeSuffixTestKb()
+        [DataTestMethod]
+        [DataRow(1024, "1.0 KB")]
+        [DataRow(1048576, "1.0 MB")]
+        [DataRow(1073741824, "1.0 GB")]
+        [DataRow(1099511627776, "1.0 TB")]
+        [DataRow(1125899906842624, "1.0 PB")]
+        [DataRow(1152921504606846976, "1.0 EB")]
+        public void SizeSuffixTest(long sizeLong, string result)
         {
             // Arrange
-            var size = DataSize.SizeSuffix(1024);
+            var size = DataSize.SizeSuffix(sizeLong);
 
             // Assert
-            Assert.AreEqual("1.0 KB", size);
-        }
-
-        [TestMethod]
-        public void SizeSuffixTestMb()
-        {
-            // Arrange
-            var size = DataSize.SizeSuffix(1048576);
-
-
-            // Assert
-            Assert.AreEqual("1.0 MB", size);
-        }
-
-        [TestMethod]
-        public void SizeSuffixTestGb()
-        {
-            // Arrange
-            var size = DataSize.SizeSuffix(1073741824);
-
-
-            // Assert
-            Assert.AreEqual("1.0 GB", size);
-        }
-
-        [TestMethod]
-        public void SizeSuffixTestTb()
-        {
-            // Arrange
-            var size = DataSize.SizeSuffix(1099511627776);
-
-
-            // Assert
-            Assert.AreEqual("1.0 TB", size);
-        }
-
-        [TestMethod]
-        public void SizeSuffixTestPb()
-        {
-            // Arrange
-            var size = DataSize.SizeSuffix(1125899906842624);
-
-
-            // Assert
-            Assert.AreEqual("1.0 PB", size);
-        }
-
-        [TestMethod]
-        public void SizeSuffixTestEb()
-        {
-            // Arrange
-            var size = DataSize.SizeSuffix(1152921504606846976);
-
-
-            // Assert
-            Assert.AreEqual("1.0 EB", size);
+            Assert.AreEqual(result, size);
         }
     }
 }

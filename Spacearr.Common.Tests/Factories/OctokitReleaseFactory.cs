b6f569071dc6e3053cb674ea870409242d988971
Teshvier Mathura <teshvierm@saratoga.co.za>
2020-07-15 10:00:33 +0200

    add UpdateService Tests
    GH-25

---
+++ Spacearr.Common.Tests/Factories/OctokitReleaseFactory.cs
@@ -0,0 +1,36 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Spacearr.Common.Tests.Factories
+{
+    public static class OctokitRelease
+    {
+        public static List<Release> CreateOctokitReleases(int total)
+        {
+            var releases = new List<Release>();
+
+            for (var i = 0; i < total; i++)
+            {
+                var releaseAsserts = new List<ReleaseAsset>
+                {
+                    new ReleaseAsset("url", 1, "1", "apk", "label", "state", "contentType",
+                        1, 1, DateTimeOffset.Now, DateTimeOffset.Now, $"browserDownloadUrl_apk_{i}", new Author()),
+                    new ReleaseAsset("url", 1, "1", "windowsservice", "label", "state", "contentType",
+                        1, 1, DateTimeOffset.Now, DateTimeOffset.Now, $"browserDownloadUrl_windowsservice_{i}", new Author()),
+                    new ReleaseAsset("url", 1, "1", "workerservice", "label", "state", "contentType",
+                        1, 1, DateTimeOffset.Now, DateTimeOffset.Now, $"browserDownloadUrl_workerservice_{i}", new Author()),
+                    new ReleaseAsset("url", 1, "1", "uwp", "label", "state", "contentType",
+                        1, 1, DateTimeOffset.Now, DateTimeOffset.Now, $"browserDownloadUrl_uwp_{i}", new Author())
+                };
+
+                releases.Add(new Release("url", "htmlUrl", "assetsUrl", "uploadUrl", 1, "nodeId", $"1.0.0.{i}", 
+                    "targetCommitish", "name", "body", false, false, DateTime.Now, DateTime.Now, new Author(), "tarballUrl", 
+                    "zipballUrl", releaseAsserts));
+            }
+
+            return releases.OrderByDescending(x => x.CreatedAt).ToList();
+        }
+    }
+}
---
+++ Spacearr.Common.Tests/Services/UpdateServiceTests.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Octokit;
+using Spacearr.Common.Enums;
+using Spacearr.Common.Logger.Interfaces;
+using Spacearr.Common.Services.Implementations;
+using Spacearr.Common.Tests.Factories;
+using System;
+using System.Threading.Tasks;
+
+namespace Spacearr.Common.Tests.Services
+{
+    [TestClass]
+    public class UpdateServiceTests
+    {
+        private Mock<ILogger> _mockILogger;
+        private Mock<IGitHubClient> _mockIGitHubClient;
+        private Version _currentVersion = new Version("1.0.0.0");
+        private UpdateService _updateService;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _mockILogger = new Mock<ILogger>();
+            _mockIGitHubClient = new Mock<IGitHubClient>();
+            _currentVersion = new Version("1.0.0.0");
+        }
+
+        [TestMethod]
+        public async Task CheckForUpdateAsync_NeedUpdate()
+        {
+            // Arrange
+            const int noOfReleases = 20;
+            var releases = OctokitRelease.CreateOctokitReleases(noOfReleases);
+            _mockIGitHubClient.Setup(x => x.Repository.Release.GetAll("tmathura", "Spacearr")).ReturnsAsync(releases);
+            _updateService = new UpdateService(_mockILogger.Object, _mockIGitHubClient.Object);
+
+            // Act
+            var update = await _updateService.CheckForUpdateAsync(_currentVersion.ToString());
+
+            // Assert
+            Assert.AreEqual(true, update);
+        }
+
+        [TestMethod]
+        public async Task CheckForUpdateAsync_DoesNotNeedUpdate()
+        {
+            // Arrange
+            const int noOfReleases = 1;
+            var releases = OctokitRelease.CreateOctokitReleases(noOfReleases);
+            _mockIGitHubClient.Setup(x => x.Repository.Release.GetAll("tmathura", "Spacearr")).ReturnsAsync(releases);
+            _updateService = new UpdateService(_mockILogger.Object, _mockIGitHubClient.Object);
+
+            // Act
+            var update = await _updateService.CheckForUpdateAsync(_currentVersion.ToString());
+
+            // Assert
+            Assert.AreEqual(false, update);
+        }
+
+        [TestMethod]
+        public async Task UpdateUrlOfLastUpdateCheck_Apk()
+        {
+            // Arrange
+            const int noOfReleases = 7;
+            var releases = OctokitRelease.CreateOctokitReleases(noOfReleases);
+            _mockIGitHubClient.Setup(x => x.Repository.Release.GetAll("tmathura", "Spacearr")).ReturnsAsync(releases);
+            _updateService = new UpdateService(_mockILogger.Object, _mockIGitHubClient.Object);
+
+            // Act
+            var update = await _updateService.CheckForUpdateAsync(_currentVersion.ToString());
+            var url = await _updateService.UpdateUrlOfLastUpdateCheck(UpdateType.Android);
+
+            // Assert
+            Assert.AreEqual(true, update);
+            Assert.AreEqual($"browserDownloadUrl_apk_{noOfReleases-1}", url);
+        }
+
+        [TestMethod]
+        public async Task UpdateUrlOfLastUpdateCheck_WindowsService()
+        {
+            // Arrange
+            const int noOfReleases = 9;
+            var releases = OctokitRelease.CreateOctokitReleases(noOfReleases);
+            _mockIGitHubClient.Setup(x => x.Repository.Release.GetAll("tmathura", "Spacearr")).ReturnsAsync(releases);
+            _updateService = new UpdateService(_mockILogger.Object, _mockIGitHubClient.Object);
+
+            // Act
+            var update = await _updateService.CheckForUpdateAsync(_currentVersion.ToString());
+            var url = await _updateService.UpdateUrlOfLastUpdateCheck(UpdateType.WindowsService);
+
+            // Assert
+            Assert.AreEqual(true, update);
+            Assert.AreEqual($"browserDownloadUrl_windowsservice_{noOfReleases-1}", url);
+        }
+
+        [TestMethod]
+        public async Task UpdateUrlOfLastUpdateCheck_WorkerService()
+        {
+            // Arrange
+            const int noOfReleases = 11;
+            var releases = OctokitRelease.CreateOctokitReleases(noOfReleases);
+            _mockIGitHubClient.Setup(x => x.Repository.Release.GetAll("tmathura", "Spacearr")).ReturnsAsync(releases);
+            _updateService = new UpdateService(_mockILogger.Object, _mockIGitHubClient.Object);
+
+            // Act
+            var update = await _updateService.CheckForUpdateAsync(_currentVersion.ToString());
+            var url = await _updateService.UpdateUrlOfLastUpdateCheck(UpdateType.WorkerService);
+
+            // Assert
+            Assert.AreEqual(true, update);
+            Assert.AreEqual($"browserDownloadUrl_workerservice_{noOfReleases-1}", url);
+        }
+
+        [TestMethod]
+        public async Task UpdateUrlOfLastUpdateCheck_Uwp()
+        {
+            // Arrange
+            const int noOfReleases = 15;
+            var releases = OctokitRelease.CreateOctokitReleases(noOfReleases);
+            _mockIGitHubClient.Setup(x => x.Repository.Release.GetAll("tmathura", "Spacearr")).ReturnsAsync(releases);
+            _updateService = new UpdateService(_mockILogger.Object, _mockIGitHubClient.Object);
+
+            // Act
+            var update = await _updateService.CheckForUpdateAsync(_currentVersion.ToString());
+            var url = await _updateService.UpdateUrlOfLastUpdateCheck(UpdateType.Uwp);
+
+            // Assert
+            Assert.AreEqual(true, update);
+            Assert.AreEqual($"browserDownloadUrl_uwp_{noOfReleases-1}", url);
+        }
+
+        [TestMethod]
+        public async Task UpdateUrlOfLastUpdateCheck_DidNotRunCheckForUpdateAsyncFirst()
+        {
+            // Arrange
+            const int noOfReleases = 15;
+            var releases = OctokitRelease.CreateOctokitReleases(noOfReleases);
+            _mockIGitHubClient.Setup(x => x.Repository.Release.GetAll("tmathura", "Spacearr")).ReturnsAsync(releases);
+            _updateService = new UpdateService(_mockILogger.Object, _mockIGitHubClient.Object);
+            
+            // Assert
+            await Assert.ThrowsExceptionAsync<Exception>(() => _updateService.UpdateUrlOfLastUpdateCheck(UpdateType.Android));
+        }
+
+        [TestMethod]
+        public async Task LatestTagName()
+        {
+            // Arrange
+            const int noOfReleases = 3;
+            var releases = OctokitRelease.CreateOctokitReleases(noOfReleases);
+            _mockIGitHubClient.Setup(x => x.Repository.Release.GetAll("tmathura", "Spacearr")).ReturnsAsync(releases);
+            _updateService = new UpdateService(_mockILogger.Object, _mockIGitHubClient.Object);
+
+            // Act
+            var update = await _updateService.CheckForUpdateAsync(_currentVersion.ToString());
+
+            // Assert
+            Assert.AreEqual(true, update);
+            Assert.AreEqual($"1.0.0.{noOfReleases-1}", _updateService.LatestTagName);
+        }
+
+        [TestMethod]
+        public void LatestTagName_DidNotRunCheckForUpdateAsyncFirst()
+        {
+            // Arrange
+            const int noOfReleases = 3;
+            var releases = OctokitRelease.CreateOctokitReleases(noOfReleases);
+            _mockIGitHubClient.Setup(x => x.Repository.Release.GetAll("tmathura", "Spacearr")).ReturnsAsync(releases);
+            _updateService = new UpdateService(_mockILogger.Object, _mockIGitHubClient.Object);
+            
+            // Assert
+            Assert.ThrowsException<Exception>(() => _updateService.LatestTagName);
+        }
+    }
+}

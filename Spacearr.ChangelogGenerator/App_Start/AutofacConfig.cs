5994c72af64b3b3a825841bd1dbdef38ca0bfd30
Teshvier Mathura <teshvierm@saratoga.co.za>
2020-07-27 16:13:20 +0200

    remove token

--- Spacearr.ChangelogGenerator/App_Start/AutofacConfig.cs
+++ Spacearr.ChangelogGenerator/App_Start/AutofacConfig.cs
@@ -9,7 +9,7 @@ namespace Spacearr.ChangelogGenerator
     {
         public static void Configure(ContainerBuilder builder)
         {
-            builder.Register(c => new GitHubClient(new ProductHeaderValue("Spacearr")) { Credentials = new Credentials("0c0edc25d3fef06a7cd3e71bb9d2b2eb92b30021") }).As<IGitHubClient>().SingleInstance();
+            builder.Register(c => new GitHubClient(new ProductHeaderValue("Spacearr")) { Credentials = new Credentials("") }).As<IGitHubClient>().SingleInstance();
             builder.RegisterType<ChangelogGeneratorService>().As<IChangelogGeneratorService>().SingleInstance();
         }
     }

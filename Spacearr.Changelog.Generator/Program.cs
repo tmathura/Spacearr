d117320cd97f5584b5a1812113c3026e44df0802
Teshvier Mathura <teshvierm@saratoga.co.za>
2020-07-28 15:43:15 +0200

    changelog generator to take current branch and update yml to cater for this

--- Spacearr.Changelog.Generator/Program.cs
+++ Spacearr.Changelog.Generator/Program.cs
@@ -9,16 +9,16 @@ namespace Spacearr.Changelog.Generator
     {
         public static async Task Main(string[] args)
         {
-            if (args.Length != 4)
+            if (args.Length != 5)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Too many arguments supplied, supply only the 'GitHub Owner', 'GitHub Repo', 'Directory Of Clone', 'GitHub Token'");
+                Console.WriteLine("Too many arguments supplied, supply only the 'GitHub Owner', 'GitHub Repo', 'Directory Of Clone', 'Current Branch', 'GitHub Token'");
                 Console.ForegroundColor = ConsoleColor.White;
             }
             else
             {
                 var builder = new ContainerBuilder();
-                AutofacConfig.Configure(args[0], args[1], args[2], args[3], builder);
+                AutofacConfig.Configure(args[0], args[1], args[2], args[3], args[4], builder);
                 var container = builder.Build();
                 var changelogGeneratorService = container.Resolve<IChangelogGeneratorService>();
                 await changelogGeneratorService.CreateChangelog();
